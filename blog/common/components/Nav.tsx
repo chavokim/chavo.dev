5d91ca3ee247690768c22ab945ff13e4630d1224
chavokim <treeboy0384@naver.com>
2022-04-21 01:46:43 +0900

    :sparkles: Added progress bar

--- blog/common/components/Nav.tsx
+++ blog/common/components/Nav.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Image from 'next/image'
-import { gsap } from "gsap/dist/gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import useIsomorphicLayoutEffect from "../utils/useIsomorphicLayoutEffect"
 import {useRouter} from "next/router";
 
@@ -34,7 +33,13 @@ const Nav: React.FC = () => {
                 navEl?.classList.remove("pt-8", "pb-4");
                 imageEl?.setAttribute("src", "/logo_typo.svg");
             },
-        })
+        });
+
+        gsap.to('progress', {
+            value: 100,
+            ease: 'none',
+            scrollTrigger: { scrub: 0.3 }
+        });
     }, [])
 
     const router = useRouter();
@@ -54,37 +59,47 @@ const Nav: React.FC = () => {
                     height: 108,
                 }}
             />
-            <header className="top-0 left-0 right-0 bg-red z-50
-             w-screen fixed pt-8 pb-4 ease-in-out transition-all">
-                <div className="container mx-auto px-4">
-                    <nav className="flex flex-row justify-between">
-                        <img
-                            className="cursor-pointer hover:opacity-70 logo-image"
-                            src="/logo_typo_animation.gif"
-                            style={{
-                                height: 50,
-                                width: "auto",
-                            }}
-                            onClick={() => router.push("/")}
-                        />
-                        <div className="flex flex-row">
-                        {
-                            TAB_ITEMS.map(tabItem => (
-                                <a
-                                    className={
-                                        `p-4 text-lg text-white font-medium 
-                                        hover:text-black ${selected(tabItem.label) ? "text-black" : ""}`
-                                    }
-                                    href={tabItem.href}
-                                >
-                                    {tabItem.label}
-                                </a>
-                            ))
-                        }
-                        </div>
-                    </nav>
-                </div>
-            </header>
+            <div className="top-0 left-0 right-0 fixed z-50 flex flex-col">
+                <header className="bg-red
+                 w-screen pt-8 pb-4 ease-in-out transition-all">
+                    <div className="container mx-auto px-4">
+                        <nav className="flex flex-row justify-between">
+                            <img
+                                className="cursor-pointer hover:opacity-70 logo-image"
+                                src="/logo_typo_animation.gif"
+                                style={{
+                                    height: 50,
+                                    width: "auto",
+                                }}
+                                onClick={() => router.push("/")}
+                            />
+                            <div className="flex flex-row">
+                            {
+                                TAB_ITEMS.map(tabItem => (
+                                    <a
+                                        className={
+                                            `p-4 text-lg text-white font-medium 
+                                            hover:text-black ${selected(tabItem.label) ? "text-black" : ""}`
+                                        }
+                                        href={tabItem.href}
+                                    >
+                                        {tabItem.label}
+                                    </a>
+                                ))
+                            }
+                            </div>
+                        </nav>
+                    </div>
+                </header>
+                <progress
+                    className="w-screen h-1"
+                    max={100}
+                    value={0}
+                    style={{
+                        background: "linear-gradient(to right, #8e0e00, #1f1c18)",
+                    }}
+                />
+            </div>
         </>
     )
 }
