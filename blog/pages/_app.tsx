06ec3e128f803843c3259e42517feb0c19b00300
chavokim <treeboy0384@naver.com>
2022-06-25 15:45:12 +0900

    fix: added git ignore

---
+++ blog/pages/_app.tsx
@@ -0,0 +1,75 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from "next/head";
+import Nav from "../common/components/Nav";
+import React from "react";
+import Footer from '../common/components/Footer';
+import Script from 'next/script';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+    <div>
+        <Head>
+            <title>Chavolog</title>
+            <meta name="description" content="Chavo Kim의 이것저것 쓰는 blog" key={"description"}/>
+            <meta name="naver-site-verification" content="ffd2270ebd7d5eaafd28a54e52a4afda91a05e1f" />
+            <link rel="icon" href="/favicon.ico" />
+            <meta property="og:site_name" content="blog.chavo.dev" key={"og-site-name"} />
+            <meta property="og:title" content="Chavolog" key={"og-title"} />
+            <meta property="og:description" content="Chavo Kim의 이것저것 쓰는 blog" key={"og-description"} />
+            <meta property="og:type" content="article" key={"og-type"} />
+            <meta property="og:image" content="/og_image.png" key={"og-image"} />
+            <meta name="google-signin-client_id" 
+                  content="724742144526-ti6ueiflj3c2eta269bjds0p0o3lmiqk.apps.googleusercontent.com"
+            />
+            <meta name="google-signin-scope" content="https://www.googleapis.com/auth/analytics.readonly"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""} />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+                rel="stylesheet"
+            />
+            <link 
+                href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@500&display=swap" 
+                rel="stylesheet"
+            />
+            <link
+                rel="stylesheet"
+                as="style"
+                crossOrigin={""}
+                href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+            />
+        </Head>
+        <Script
+            id="gtag-script"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-33PFC5KLH1`}
+        />
+        <Script
+            strategy="afterInteractive"
+            id="gtag-script2"
+            dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'G-33PFC5KLH1');
+            `,
+            }}
+        />
+        <Script 
+            src="https://apis.google.com/js/client:platform.js"
+        />
+        <Nav />
+        <section className="py-24 width-screen dark:bg-black">
+            <div className="xl:max-w-5xl container mx-auto ">
+                <Component {...pageProps} />
+            </div>
+        </section>
+        <Footer />
+    </div>
+    )
+}
+
+export default MyApp
---
+++ blog/pages/blog/[slug].tsx
@@ -0,0 +1,104 @@
+import Head from "next/head";
+import {GetStaticPaths, GetStaticProps, NextPage} from "next";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import {serialize} from "next-mdx-remote/serialize";
+import {MDXRemote, MDXRemoteSerializeResult} from "next-mdx-remote";
+import {FrontMatterType} from "../../common/interfaces/post";
+import React from "react";
+import moment from "moment";
+import Image from "next/image";
+import remarkGfm from "remark-gfm";
+import DisqusComments from "../../common/components/DisqusComments";
+
+interface PostPageTypeProps {
+    frontMatter: FrontMatterType,
+    mdxSource: MDXRemoteSerializeResult,
+    slug: string,
+}
+
+const PostPage: NextPage<PostPageTypeProps> = ({frontMatter, mdxSource, slug}) => {
+    const {title, date, image, excerpt} = frontMatter; 
+    
+    return (
+        <>
+            <Head>
+                <title>Chavolog - {title}</title>
+                <meta name="description" content={excerpt} key={"description"} />
+                <meta property="og:title" content={title} key={"og-title"} />
+                <meta property="og:description" content={excerpt} key={"og-description"} />
+                <meta property="og:image" content={image} key={"og-image"} />
+            </Head>
+            <div className="flex flex-col items-center px-4 sm:px-0">
+                <article className={"prose overflow-hidden dark:prose-invert"}>
+                    <div className="flex flex-col items-center">
+                        <h1>{title}</h1>
+                        <p className="m-0 font-normal text-gray-500 mb-12">
+                            {moment(date).format("ll")}
+                        </p>
+                        <div className="w-full aspect-video relative rounded-lg overflow-hidden">
+                            <Image
+                                src={image}
+                                alt="thumbnail"
+                                layout={"fill"}
+                                objectFit="cover"
+                            />
+                        </div>
+                    </div>
+                    <MDXRemote {...mdxSource} components={{ Image }} />
+                </article>
+                <div
+                    className="mt-24 w-full"
+                >
+                    <DisqusComments
+                        frontMatter={frontMatter}
+                        slug={slug}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const files = fs.readdirSync(path.join("posts"));
+
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace(".mdx", "")
+        }
+    }));
+    
+    return ({
+        paths,
+        fallback: false,
+    })
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const slug = context.params?.slug;
+
+    const markdownWithMeta = fs.readFileSync(path.join("posts", slug + ".mdx"), "utf-8");
+
+    const {data: frontMatter, content} = matter(markdownWithMeta);
+
+    const mdxSource = await serialize(content, {
+        mdxOptions: {
+            remarkPlugins: [
+                remarkGfm, 
+                require('remark-prism'),
+            ]
+        },
+    });
+
+    return ({
+        props: {
+            frontMatter,
+            slug,
+            mdxSource
+        }
+    })
+}
+
+export default PostPage;
