4ef4dbf8ff882aef9df256f4e95b349e00855e4f
chavokim <treeboy0384@naver.com>
2022-06-28 00:11:13 +0900

    feat: Added sitemap generator

---
+++ blog/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: process.env.SITE_URL || 'https://blog.chavo.dev',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
+  export default config
--- blog/next.config.js
+++ blog/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
+    if(isServer) {
+      require("./scripts/sitemap-generator");
+    }
     return config
   },
 }
