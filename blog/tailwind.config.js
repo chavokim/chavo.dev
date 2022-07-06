c41752648b7dc5130735ec6974095713be706eb2
chavokim <treeboy0384@naver.com>
2022-07-07 04:29:11 +0900

    style: Enlarge post max-width

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -7,18 +7,25 @@ module.exports = {
     "./common/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-    extend: {
-        colors: {
-          red: "#D50606",
-          white: "#F2F2F2",
-          black: "#1A1A1A",
-          pureBlack: "#000000",
-          pureWhite: "#FFFFFF",
-        },
-        fontFamily: {
-            mono: ["Roboto Mono", ...defaultTheme.fontFamily.mono],
-        },
-    },
+      extend: {
+          colors: {
+            red: "#D50606",
+            white: "#F2F2F2",
+            black: "#1A1A1A",
+            pureBlack: "#000000",
+            pureWhite: "#FFFFFF",
+          },
+          fontFamily: {
+              mono: ["Roboto Mono", ...defaultTheme.fontFamily.mono],
+          },
+          typography: {
+            DEFAULT: {
+              css: {
+                maxWidth: "80ch"
+              },
+            },
+          },
+      },
     },
     plugins: [
       require("@tailwindcss/typography"),
